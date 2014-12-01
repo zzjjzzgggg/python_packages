abee63b976c97339b394a634d4de3bdd9e85d650
jzzhao@laptop <jzzhao@sei.xjtu.edu.cn>
2014-12-01 16:57:14 +0800

    fix bugs.

--- specialfunctions.py
+++ specialfunctions.py
@@ -5,11 +5,15 @@ import math
 def nchoosek(n,k):
 	return math.exp(math.lgamma(n+1)-math.lgamma(k+1)-math.lgamma(n-k+1))
 
-def beta_binomial(x,n,alpha,beta):
-	i1 = math.lgamma(-alpha+1) - math.lgamma(x+1) - math.lgamma(-alpha-x+1)
-	i2 = math.lgamma(-beta+1) - math.lgamma(n-x+1) - math.lgamma(-beta-n+x+1)
-	i3 = math.lgamma(-alpha-beta+1) - math.lgamma(n+1) - math.lgamma(-alpha-beta-n+1)
-	return math.exp(i1+i2-i3)
+#                         T(n+1)     T(k+alpah)T(n-k+beta)  T(alpha+beta)
+# f(k|n,alpha,beta) = --------------.---------------------.---------------
+#                     T(k+1)T(n-k+1)    T(n+alpha+beta)    T(alpha)T(beta)
+def beta_binomial(k,n,alpha,beta):
+	i1 = math.lgamma(n+1) - math.lgamma(k+1) - math.lgamma(n-k+1)
+	i2 = math.lgamma(k+alpha) + math.lgamma(n-k+beta) - math.lgamma(n+alpha+beta)
+	i3 = math.lgamma(alpha+beta) - math.lgamma(alpha) - math.lgamma(beta)
+	return math.exp(i1+i2+i3)
 
 if __name__=='__main__':
-	print(sum([beta_binomial(i,10,0.4,0.5) for i in range(11)]))
+	N = 20
+	print(sum([beta_binomial(i,N,0.27,9.73) for i in range(N+1)]))
