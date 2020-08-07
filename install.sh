e47ce1874f07a21496b270bb2245baea46daabad
J. Zhao <zzjjzzgggg@gmail.com>
2020-08-07 17:06:56 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# created on 2020-08-07 17:04
+
+# Make sure pip is installed. For ubuntu, install pip by
+#   sudo apt install python3-pip
+
+
+pip3 install -e iotools
+pip3 install -e myutils
+pip3 install -e simplestat
